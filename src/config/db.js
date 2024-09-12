42c5b491271a5d7ceb3604bbad166f81ce091490
10-Yerson <Dreyerg9@gmail.com>
2024-09-12 10:36:31 -0500

    BD Nube MongoAtlas

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb://localhost:27017/auth-system');
+        await mongoose.connect('mongodb+srv://Momentary:7s5AAj9iRjiXRgRK@socialnest.sqee9.mongodb.net/?retryWrites=true&w=majority&appName=SocialNest');
         console.log('MongoDB Conectado');
     } catch (err) {
         console.error('Error al conectara MongoDB...', err);
